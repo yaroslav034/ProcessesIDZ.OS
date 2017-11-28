35ad7b75329cb87bd5f36707c9da6bb856bb8987
Jason Bazooka <yarikbosenko@gmail.com>
2017-11-28 11:45:00 +0200

    Initial Commit

---
+++ ProcessesIDZ.OS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProcessesIDZ.OS
+//
+//  Created by Yaroslav Bosenko on 28.11.2017.
+//  Copyright © 2017 Yaroslav Bosenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
